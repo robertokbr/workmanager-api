59ffaaf0a6513ab1a761a10f357a9c1c560a9984
robertokbr <robertojuniordev@gmail.com>
2021-02-06 12:06:29 -0300

    Change db select on init

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ app.get('/', async (_, response) => {
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM users');
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
     client.release();
