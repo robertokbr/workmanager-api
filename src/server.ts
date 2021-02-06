93c18460d0bcdad737b52c1b415d0adb27ed77fa
robertokbr <robertojuniordev@gmail.com>
2021-02-06 09:15:23 -0300

    Setup db entrypoint

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,17 @@ import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import './database';
+import { Pool } from 'pg';
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 app.use(cors());
 
@@ -17,6 +25,19 @@ app.use(routes);
 app.get('/', async (_, response) => {
   return response.json({ connected: true });
 });
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
